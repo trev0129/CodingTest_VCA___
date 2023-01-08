15853f9b22e2358fe88bb8127f6896a484f64e17
trev0129 <pbm9393@gmail.com>
2023-01-08 14:42:20 +0900

    Test 문제 업로드

--- src/main/java/com/example/test/quiz04/Test04.java
+++ src/main/java/com/example/test/quiz04/Test04.java
@@ -2,20 +2,17 @@ package com.example.test.quiz04;
 
 public class Test04 {
   private static int factorial(int num) {
-    if (num > Integer.MAX_VALUE) {
-      return 0;
-    } else if (num == 1) {
+    if (num == 1) {
       return 1;
-    } else {
-      return num * factorial(--num);
     }
+    return num * factorial(--num);
   }
   public static void main(String[] args) {
-    int result = factorial(50);
-    if (result == 0) {
-      System.out.println("스택오버플로우!");
-    } else {
+    try {
+      int result = factorial(1000000);
       System.out.println(result);
+    } catch (StackOverflowError e) {
+      System.out.println("스택오버플로우!");
     }
   }
 }
